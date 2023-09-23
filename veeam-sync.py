490784084838208a3b908c6d1e453d34bcba5b9e
Johannes Alexis Wirde <johannes.wirde@gmail.com>
2023-09-23 14:05:56 +0200

    Renaming start to main.

--- veeam-sync.py
+++ veeam-sync.py
@@ -4,10 +4,10 @@ import click
 @click.option('--logfile', type=str, required=True)
 @click.option('--source', type=str, required=True)
 @click.option('--destination', type=str, required=True)
-def start(logfile, source, destination):
+def main(logfile, source, destination):
     with open(logfile, 'w') as log:
         print("Logfile found and write permission granted.")
         log.write("Logfile found and write permission granted.")
 
 if __name__ == '__main__':
-    start()
+    main()
