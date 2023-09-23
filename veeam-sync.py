3f6463f8cd924529e95f84a5aa65e11a200348f8
Johannes Alexis Wirde <johannes.wirde@gmail.com>
2023-09-23 14:22:12 +0200

    Adding tell function

--- veeam-sync.py
+++ veeam-sync.py
@@ -1,13 +1,22 @@
 import click
+import os
+import datetime
+
+log = None
+def tell(msg):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+    print(timestamp + " " + msg)
+    log.write(timestamp + " " + msg)
 
 @click.command()
 @click.option('--logfile', type=str, required=True)
 @click.option('--source', type=str, required=True)
 @click.option('--destination', type=str, required=True)
 def main(logfile, source, destination):
-    with open(logfile, 'w') as log:
-        print("Logfile found and write permission granted.")
-        log.write("Logfile found and write permission granted.")
+    global log
+    log = open(logfile, 'w')
+    tell("Logfile found and write permission granted.")
 
 if __name__ == '__main__':
     main()
